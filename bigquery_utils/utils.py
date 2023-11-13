ad9a667423c3a55719681097f22bb460698012e6
PeDiot <p.emmanuel.diot@gmail.com>
2023-11-13 12:17:41 +0100

    add get_table_path

--- bigquery_utils/utils.py
+++ bigquery_utils/utils.py
@@ -1,6 +1,7 @@
 from typing import List
 from google.cloud import bigquery
 import pandas as pd
+from .enums import PROJECT
 
 
 def get_dataset_ids(client: bigquery.Client) -> List:
@@ -17,6 +18,10 @@ def get_table_ids(client: bigquery.Client, dataset_id: str) -> List:
     table_ids = [table.table_id for table in tables]
 
     return table_ids
+
+def get_table_path(dataset_id: str, table_id: str) -> str: 
+
+    return f'{PROJECT}.{dataset_id}.{table_id}'
 
 def query_to_pandas(client: bigquery.Client, query: str) -> pd.DataFrame: 
 
