bbead83820e34a2866a8d0828402624e93c6de58
PeDiot <p.emmanuel.diot@gmail.com>
2023-11-16 14:30:58 +0100

    change credential scopes

--- bigquery_utils/session.py
+++ bigquery_utils/session.py
@@ -2,9 +2,16 @@ from google.cloud import bigquery
 import os
 
 
+SCOPES = [
+    'https://www.googleapis.com/auth/cloud-platform',
+    'https://www.googleapis.com/auth/drive',
+    "https://www.googleapis.com/auth/bigquery"
+]
+
 def create_client(credential_path: str) -> bigquery.Client:
     
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
     client = bigquery.Client()
+    client._credentials._scopes = SCOPES
 
     return client
